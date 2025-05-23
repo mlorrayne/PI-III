192ff1a380827701d40945446ae1d9c70fbb9a64
mlorrayne <lorrayne_pego@yahoo.com.br>
2025-05-22 23:39:49 -0300

    refatoração

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('historia/', views.historia, name='historia'),
+    path('contato/', views.contato, name='contato'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def historia(request):
+    return render(request, 'core/historia.html')
+
+def contato(request):
+    return render(request, 'core/contato.html')
+
